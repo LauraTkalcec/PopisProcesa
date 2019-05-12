1dc914a12773c175a7ef6f99df512ec0e3cce603
LauraTkalcec <tkalcec.laura@gmail.com>
2019-05-12 15:08:33 +0200

    Popis 10 procesa

--- PopisProcesa/Program.cs
+++ PopisProcesa/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Data;
 
 namespace PopisProcesa
 {
@@ -10,6 +12,33 @@ namespace PopisProcesa
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n    Popis 10 procesa:    \n");
+            Process[] procesi = Process.GetProcesses();
+
+            //ispisati popis procesa
+            var popisProcesa = System.Diagnostics.Process.GetProcesses();
+            var proces = popisProcesa[0];
+            for(int j=0;j<popisProcesa.Length;j++)
+            {
+                for(int i=0;i<popisProcesa.Length;i++)
+                {
+                    var p1 = popisProcesa[i];
+                    var p2 = popisProcesa[j];
+
+                    if(popisProcesa[i].WorkingSet64>popisProcesa[j].WorkingSet64)
+                    {
+                        var drzi = popisProcesa[i];
+                        popisProcesa[i] = popisProcesa[j];
+                        popisProcesa[j] = drzi;
+                    }
+                }
+            }
+
+            for (int i=0; i<10; i++)
+            {
+                var p = popisProcesa[i];
+                Console.WriteLine("{0}-{1}", p.WorkingSet64, p.ProcessName);
+            }
         }
     }
 }
